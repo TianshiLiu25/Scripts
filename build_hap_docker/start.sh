706f8f33715a04f1bd45001a80475d2ca11f41e7
Tianshi Liu <tianshiliu25@gmail.com>
2022-06-06 23:01:06 +0800

    update

--- build_hap_docker/start.sh
+++ build_hap_docker/start.sh
@@ -6,4 +6,4 @@ cd /root/oh_hap_to_build
 npm install
 npm run buildOhosHaps
 find . -type f | grep -E '\.hap' | xargs realpath
-/root/tools/sign_tool/sign_hap.sh /root/oh_hap_to_build/entry/build/default/outputs/default/entry-default-unsigned.hap /root/oh_hap_to_build/FaceAuthHap.hap 'com.ohos.settings.faceauth'
+/root/tools/sign_tool/sign_hap.sh /root/oh_hap_to_build/entry/build/default/outputs/default/entry-default-unsigned.hap /root/oh_hap_to_build/Settings_FaceAuth.hap 'com.ohos.settings.faceauth'
